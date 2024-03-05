3612128e607301afa72fb74ae2de6c131317576d
Rafael Brandão <raffaelbrandao@gmail.com>
2024-03-05 20:44:14 -0300

    Update import.sql

--- demo/src/main/resources/import.sql
+++ demo/src/main/resources/import.sql
@@ -1,7 +1,3 @@
-insert into Item (id, name, category, price) values (100, 'Back to the Future', 'Movies', '50');
-insert into Item (id, name, category, price) values (200, 'Stand by me', 'Music', '10');
-insert into Item (id, name, category, price) values (300, 'TreadMill', 'Health', '1500'); 
-
 insert into Person (id, name, birthday, gender) values (100, 'Rafael', '1989-05-02', 'M');
 insert into Person (id, name, birthday, gender) values (101, 'Maria', '1989-05-03', 'F');
-insert into Person (id, name, birthday, gender) values (102, 'jOAO', '1989-05-06', 'M');
+insert into Person (id, name, birthday, gender) values (102, 'Pedro', '1989-05-06', 'M');
